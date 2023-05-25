4482683c425323101a1d9077e49daeb9e1f63305
Catharin <jekaterina.shashkina@gmail.com>
2023-05-25 21:27:53 +0300

    modal open close

--- index.js
+++ index.js
@@ -71,3 +71,16 @@ const renderGoods = (arr) => {
 };
 
 renderGoods(dataArray);
+
+const addGoods = document.querySelector(".panel__add-goods");
+const closeBtn = document.querySelector(".modal__close");
+addGoods.addEventListener("click", () => {
+  overlay.classList.add("active");
+});
+overlay.addEventListener("click", (e) => {
+  const target = e.target;
+  console.log(target);
+  if (target === overlay || target.closest(closeBtn)) {
+    overlay.classList.remove("active");
+  }
+});
